61b5497c3bd834711486a2795ab5ae8fd34a515e
Bruno <bacdvale@gmail.com>
2024-07-15 18:06:57 +0100

    Make nats configurable

--- config/config.go
+++ config/config.go
@@ -1,42 +1,43 @@
 package config
 
 type Config struct {
-	Server *ServerConfig `json:"server"`
-	Price  *PriceConfig  `json:"price"`
-	DB     *DBConfig     `json:"db"`
-	Nats   *NatsConfig   `json:"nats"`
-	Poets  []*PoetConfig `json:"poets"`
+    Server *ServerConfig `json:"server"`
+    Price  *PriceConfig  `json:"price"`
+    DB     *DBConfig     `json:"db"`
+    Nats   *NatsConfig   `json:"nats"`
+    Poets  []*PoetConfig `json:"poets"`
 }
 
 type PriceConfig struct {
-	Provider    string `json:"provider"`
-	RefreshTime int    `json:"refreshTime"`
+    Provider    string `json:"provider"`
+    RefreshTime int    `json:"refreshTime"`
 }
 
 type ServerConfig struct {
-	Port string `json:"port"`
+    Port string `json:"port"`
 }
 
 type NatsConfig struct {
-	Uri string `json:"uri"`
+    Enabled bool   `json:"enabled"`
+    Uri     string `json:"uri"`
 }
 
 type DBConfig struct {
-	Uri string `json:"uri"`
+    Uri string `json:"uri"`
 }
 
 type PoetConfig struct {
-	Name     string        `json:"name"`
-	Info     *PoetInfo     `json:"info"`
-	Settings *PoetSettings `json:"settings"`
+    Name     string        `json:"name"`
+    Info     *PoetInfo     `json:"info"`
+    Settings *PoetSettings `json:"settings"`
 }
 
 type PoetInfo struct {
-	Description string `json:"description"`
-	DiscordLink string `json:"discord-link"`
+    Description string `json:"description"`
+    DiscordLink string `json:"discord-link"`
 }
 
 type PoetSettings struct {
-	PhaseShift int `json:"phase-shift"`
-	CycleGap   int `json:"cycle-gap"`
+    PhaseShift int `json:"phase-shift"`
+    CycleGap   int `json:"cycle-gap"`
 }
